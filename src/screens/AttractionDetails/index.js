f81fcc68929dcd7027d1dcb248355617075953e3
PremR <prem.rajah@gmail.com>
2024-05-15 12:32:33 +0100

    added image background

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { SafeAreaView, Text } from "react-native"
+import { ImageBackground, SafeAreaView, Text } from "react-native"
 import styles from "./styles"
 import { useNavigation } from "@react-navigation/native";
 
 
 const AttractionDetailsScreen = ({ route }) => {
     const { item } = route?.params || {};
+    const mainImage = item?.images?.length ? item?.images[0] : null;
     const navigation = useNavigation();
 
     const onBack = () => {
@@ -14,7 +15,13 @@ const AttractionDetailsScreen = ({ route }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text onPress={() => onBack()} style={{ margin: 34 }}>BACK</Text>
+            <ImageBackground
+                source={{ uri: mainImage }}
+                imageStyle={{ borderRadius: 20 }}
+                style={styles.mainImage}
+            >
+                <Text onPress={() => onBack()} style={{ margin: 34 }}>BACK</Text>
+            </ImageBackground>
             <Text>{item.name}</Text>
         </SafeAreaView>
     )
--- src/screens/AttractionDetails/styles.js
+++ src/screens/AttractionDetails/styles.js
@@ -1,7 +1,15 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const { height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-
+    container: {
+        margin: 32
+    },
+    mainImage: {
+        width: '100%',
+        height: height / 2,
+    }
 });
 
 export default styles;
