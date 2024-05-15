721a382ec0084c7a1ad3cb72398795a0c5aee622
PremR <prem.rajah@gmail.com>
2024-05-15 11:57:24 +0100

    hide header on all screens

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Attraction" component={AttractionDetails} />
         <Stack.Screen name="Gallery" component={Gallery} />
