8fd4550243c615366eb12e0da65121cf8208dfeb
PremR <prem.rajah@gmail.com>
2024-05-16 11:50:53 +0100

    added gallery

--- src/screens/Gallery/index.js
+++ src/screens/Gallery/index.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { SafeAreaView, Text } from "react-native"
+import { FlatList, Image, SafeAreaView, Text, View } from "react-native"
 import styles from "./styles"
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 
 const GalleryScreen = ({ route }) => {
-    const { images } = route.params;
+    const { images } = route.params || {};
+    const navigation = useNavigation();
+
+    const goBack = () => {
+        navigation.goBack();
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Hello Gallery</Text>
+            <View style={styles.backContainer}>
+                <TouchableOpacity onPress={goBack} hitSlop={32}>
+                    <Image source={require("../../assets/back.png")} style={styles.backIcon} />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <FlatList style={{ paddingHorizontal: 24, marginBottom: 24 }} data={images} renderItem={({ item }) => (
+                    <Image source={{ uri: item }} style={styles.image} />
+                )} />
+            </View>
         </SafeAreaView>
     )
 }
--- src/screens/Gallery/styles.js
+++ src/screens/Gallery/styles.js
@@ -1,7 +1,23 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        height: "100%"
+    },
+    image: {
+        width: "100%",
+        height: 400,
+        borderRadius: 20,
+        marginTop: 4,
+    },
+    backContainer: {
+        margin: 16,
+    },
+    backIcon: {
+        width: 26,
+        height: 26,
+    }
 });
 
 export default styles;
